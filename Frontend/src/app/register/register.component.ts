b19b885585878ec967d01cf06a3059e9ac447b0c
Aaron Londot <a.londot@stag.technofuturtic.education>
2025-06-16 10:41:47 +0200

    ✨Commit 8✨ === Frontend === - Adding register form - Homepage beginning - Routing systeme updated
    === Backend ===

---
+++ Frontend/src/app/register/register.component.ts
@@ -0,0 +1,70 @@
+import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators
+} from '@angular/forms';
+import {NgClass, NgIf} from '@angular/common';
+
+export const passwordMatchValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const password = control.get('password');
+  const confirmPassword = control.get('confirmPassword');
+
+  return password && confirmPassword && password.value !== confirmPassword.value
+    ? { passwordMismatch: true }
+    : null;
+};
+
+@Component({
+  selector: 'app-register',
+  imports: [
+    ReactiveFormsModule,
+    NgClass,
+    NgIf
+  ],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  registerForm!: FormGroup;
+  submitted = false;
+  showPassword = false;
+  showConfirmPassword = false;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    }, {
+      validators: passwordMatchValidator
+    });
+  }
+
+  get f() { return this.registerForm.controls; }
+
+  togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+  toggleConfirmPasswordVisibility(): void {
+    this.showConfirmPassword = !this.showConfirmPassword;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    console.log('Données du formulaire:', this.registerForm.value);
+  }
+}
