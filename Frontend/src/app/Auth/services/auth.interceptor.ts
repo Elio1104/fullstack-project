aeb1fc4b9e6c9e77f48454983f6fe8a3917fc4f5
Aaron Londot <a.londot@stag.technofuturtic.education>
2025-06-18 19:58:54 +0200

    ✨Commit✨ === Frontend === - Interceptor update - Files management ( adding login and register to Components dir in Auth module )
    === Backend ===

--- Frontend/src/app/Auth/services/auth.interceptor.ts
+++ Frontend/src/app/Auth/services/auth.interceptor.ts
@@ -1,5 +1,20 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import {AuthService} from './auth.service';
+import {inject} from '@angular/core';
+import {Router} from '@angular/router';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService : AuthService = inject(AuthService);
+  const router : Router = inject(Router);
+  const token = localStorage.getItem('auth_token');
+
+  if (token) {
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+  }
+
   return next(req);
 };
